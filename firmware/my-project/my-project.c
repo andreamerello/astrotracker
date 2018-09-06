3d891b9130c59f98dbf6152f6c4799d93181777d
Andrea Merello <andrea.merello@gmail.com>
2018-09-06 22:39:06 +0200

    hack hack until we have a minimal blinking example

--- firmware/my-project/my-project.c
+++ firmware/my-project/my-project.c
@@ -1,6 +1,20 @@
-#include "api.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define RCC_LED RCC_GPIOC
+#define PIN_LED GPIO13
+#define PORT_LED GPIOC
 
 int main(void) {
-	/* add your own code */
-	return my_func(3);
+        rcc_periph_clock_enable(RCC_LED);
+	gpio_set_mode(PORT_LED, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED);
+	gpio_set(PORT_LED, PIN_LED);
+
+	while(1) {
+		/* wait a little bit */
+		for (int i = 0; i < 10000000; i++) {
+			__asm__("nop");
+		}
+		gpio_toggle(PORT_LED, PIN_LED);
+	}
 }
