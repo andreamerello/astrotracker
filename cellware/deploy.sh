691d296ddff8682af9d584e26f01335cda4d473e
Antonio Cuni <anto.cuni@gmail.com>
2019-12-24 14:53:29 +0100

    add a script to deploy the app to the phone via adb

---
+++ cellware/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# usage: deploy.sh [--all]
+
+TMPDIR=/tmp/adbpush/astro
+FILES=(android.txt
+       main.py
+       astro
+       libs
+      )
+
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
+
+if [ x$1 = "x--all" ]
+then
+    # deploy everything -- slowish
+    adb shell rm -rf /sdcard/kivy/astro
+    cp -L -r "${FILES[@]}" $TMPDIR
+    cd $TMPDIR
+    py.cleanup
+    cd -
+    adb push $TMPDIR /sdcard/kivy/astro
+else
+    # deploy only astro -- faster
+    cp -L -r astro $TMPDIR
+    cd $TMPDIR
+    py.cleanup
+    cd -
+    adb shell rm -rf /sdcard/kivy/astro/astro
+    adb push $TMPDIR/astro /sdcard/kivy/astro
+fi
+
+adb logcat -c
+adb logcat -s python
