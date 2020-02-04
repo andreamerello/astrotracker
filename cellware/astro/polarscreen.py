5125908da6b693ed970fb19764df0eb945ac347e
Antonio Cuni <anto.cuni@gmail.com>
2020-02-04 11:41:33 +0100

    modify RemoteCamera until it can read yuv data from the polaris server

--- cellware/astro/polarscreen.py
+++ cellware/astro/polarscreen.py
@@ -7,8 +7,11 @@ class PolarScreen(Screen):
     stars_angle = NumericProperty(0)
     camera = ObjectProperty(RemoteCamera())
 
-    # def fakeimg(self):
-    #     with open("fakeimg.png", "rb") as f:
-    #         stream = io.BytesIO(f.read())
-    #         img = CoreImage(stream, ext="png")
-    #         self.piframe_texture = img.texture
+    ## def xxx(self):
+    ##     from kivy.graphics.texture import Texture
+    ##     with open('frame.yuv', 'rb') as f:
+    ##         data = f.read()
+    ##     data = data[::-1]
+    ##     texture = Texture.create(size=(320, 240))
+    ##     texture.blit_buffer(data, colorfmt='luminance', bufferfmt='ubyte')
+    ##     self.camera.frame_texture = texture
--- cellware/astro/remotecamera.py
+++ cellware/astro/remotecamera.py
@@ -8,6 +8,15 @@ from kivy.properties import (StringProperty, ObjectProperty, NumericProperty)
 from kivy.logger import Logger
 from kivy.clock import Clock, mainthread
 from kivy.core.image import Image as CoreImage
+from kivy.graphics.texture import Texture
+
+def yuv_to_texture(data, width, height):
+    # it seems that the pixel sequence coming from picamera is
+    # updside-down compared to the one expected by Texture. Fix it!
+    data = data[::-1]
+    texture = Texture.create(size=(width, height))
+    texture.blit_buffer(data, colorfmt='luminance', bufferfmt='ubyte')
+    return texture
 
 class RemoteCamera(EventDispatcher):
     app = ObjectProperty()
@@ -57,43 +66,62 @@ class RemoteCamera(EventDispatcher):
         img = CoreImage(stream, ext="jpg")
         self.frame_texture = img.texture
 
+    @mainthread
+    def set_yuv(self, data, width, height):
+        self.frame_texture = yuv_to_texture(data, width, height)
 
     # ==============================
     # RemoteCamera thread
     # ==============================
 
-
     def run(self):
         CHUNK_SIZE = 1024
+        W, H, fps = 320, 240, 10
+        ## W, H, fps = 640, 480,  5
+        ## W, H, fps = 2592, 1944, 1
+
+        #W, H = raw_resolution(W, H) # XXX
+        path = 'camera?w=%s&h=%s&fps=%s' % (W, H, fps)
+        FRAME_SIZE = W*H # XXX: "raw_resolution"?
+
         Logger.info('RemoteCamera: thread started')
         self.set_status('Connecting...')
-        resp = requests.get(self.url('sky.mjpg'), stream=True)
+        resp = requests.get(self.url(path), stream=True)
+        #resp = requests.get(self.url('sky.mjpg'), stream=True)
         resp.raise_for_status() # XXX: handle this
         self.set_status('Connected')
 
         data = ''
-        t1 = time.time()
+        tstart = time.time()
         while self.running:
             data += resp.raw.read(CHUNK_SIZE)
-            a = data.find('\xff\xd8') # jpg_start
-            b = data.find('\xff\xd9') # jpg_end
-            if a != -1 and b != -1:
-                # found a new frame!
-                jpg_data = data[a:b+2]
-                data = data[b+2:]
-                # with open('/tmp/foo%d.jpg' % self.frame_no, 'wb') as f:
-                #     f.write(jpg_data)
-                self.set_jpg(jpg_data)
-                self.frame_no += 1
-                t2 = time.time()
-                #Logger.info('RemoteCamera: %.2f fps' % (self.frame_no / (t2-t1)))
+            if len(data) > FRAME_SIZE:
+                # got a full frame
+                yuv_data = data[:FRAME_SIZE]
+                data = data[FRAME_SIZE:]
+                self.got_frame(tstart)
+                self.set_yuv(yuv_data, W, H)
 
-                # sleep a bit: for the polaris stream this is not an issue since
-                # it will be at a very low fps. However, if you try to display a
-                # MJPG with an unbounded framerate, if you don't put the sleep
-                # you put too many tasks into kivy's @mainthread and things
-                # seems to hang.
-                time.sleep(0.01)
+            ## a = data.find('\xff\xd8') # jpg_start
+            ## b = data.find('\xff\xd9') # jpg_end
+            ## if a != -1 and b != -1:
+            ##     # found a new frame!
+            ##     jpg_data = data[a:b+2]
+            ##     data = data[b+2:]
+            ##     # with open('/tmp/foo%d.jpg' % self.frame_no, 'wb') as f:
+            ##     #     f.write(jpg_data)
 
         self.set_status('Stopped')
         Logger.info('RemoteCamera: stop')
+
+    def got_frame(self, tstart):
+        self.frame_no += 1
+        t = time.time()
+        fps = self.frame_no / (t-tstart)
+        Logger.info('RemoteCamera: %.2f fps' % fps)
+        # sleep a bit: for the polaris stream this is not an issue since
+        # it will be at a very low fps. However, if you try to display a
+        # MJPG with an unbounded framerate, if you don't put the sleep
+        # you put too many tasks into kivy's @mainthread and things
+        # seems to hang.
+        time.sleep(0.01)
