4ce7723f8172f24ba6aa51cee250a2aeae1095cb
Antonio Cuni <anto.cuni@gmail.com>
2020-02-05 15:32:44 +0100

    very advanced stuff: implement color clipping using opengl shaders :)

--- cellware/astro/polarscreen.py
+++ cellware/astro/polarscreen.py
@@ -1,6 +1,7 @@
 import io
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty, NumericProperty
+from kivy.properties import ObjectProperty, NumericProperty, BoundedNumericProperty
+from kivy.uix.effectwidget import EffectWidget, AdvancedEffectBase
 from astro.remotecamera import RemoteCamera
 from astro.uix import MyScreen
 
@@ -25,3 +26,49 @@ class PolarScreen(MyScreen):
             shutter = shutter[:-1]
             params += '?shutter=%s' % shutter
         self.camera.start(params, recording)
+
+
+
+effect_string = '''
+// the uniforms are in the range 0.0-1.0
+uniform float black;
+uniform float white;
+
+float clip(float x)
+{
+    if (x <= black) return 0.0;
+    if (x >= white) return 1.0;
+    return x;
+}
+
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    float red = clip(color.x);
+    float green = clip(color.y);
+    float blue = clip(color.z);
+    return vec4(red, green, blue, color.w);
+}
+'''
+
+class ColorClippingEffect(AdvancedEffectBase):
+    def __init__(self, *args, **kwargs):
+        super(ColorClippingEffect, self).__init__(*args, **kwargs)
+        self.glsl = effect_string
+        self.uniforms = {'black': 0.0, 'white': 1.0}
+
+
+class ColorClippingWidget(EffectWidget):
+    # the user properties are in the range 0-255
+    black = BoundedNumericProperty(0, min=0, max=255, errorvalue=0)
+    white = BoundedNumericProperty(255, min=0, max=255, errorvalue=255)
+
+    def __init__(self, *args, **kwargs):
+        super(ColorClippingWidget, self).__init__(*args, **kwargs)
+        self.clip_effect = ColorClippingEffect()
+        self.effects = [self.clip_effect]
+
+    def on_black(self, *args):
+        self.clip_effect.uniforms['black'] = self.black/255.0
+
+    def on_white(self, *args):
+        self.clip_effect.uniforms['white'] = self.white/255.0
