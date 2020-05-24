6c611f815410bc11194ca454319fbd2495c0fce4
Antonio Cuni <anto.cuni@gmail.com>
2020-05-24 23:17:08 +0200

    hack hack until we can resize the image freely, and get touch event in the coordinate system of the image itself

--- cellware/astro/rotationscreen.py
+++ cellware/astro/rotationscreen.py
@@ -7,4 +7,17 @@ from astro.error import MessageBox
 Builder.load_file(resource_find('astro/rotationscreen.kv'))
 
 class RotationScreen(MyScreen):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(RotationScreen, self).__init__(*args, **kwargs)
+        self.ids.sky.bind(on_touch_down=self.on_sky_touch_down)
+
+    def autoscale(self):
+        tw, th = self.ids.sky.texture.size
+        scale_x = self.width / float(tw)
+        scale_y = self.height / float(th)
+        self.ids.scatter.scale = min(scale_x, scale_y)
+        self.ids.scatter.pos = (0, 0)
+
+    def on_sky_touch_down(self, img, touch):
+        print touch
