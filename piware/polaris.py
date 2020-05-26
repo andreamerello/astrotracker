ec54085e14a6f671095b10b73e20bbe5116b1308
Antonio Cuni <anto.cuni@gmail.com>
2020-05-26 14:47:58 +0100

    WIP: move the picam logic into its own file

--- piware/polaris.py
+++ piware/polaris.py
@@ -5,24 +5,7 @@ import subprocess
 import os
 from urllib.parse import parse_qs
 from gphotocam import GPhotoCamera
-
-# stolen from picamera
-def raw_resolution(width, height, splitter=False):
-    """
-    Round a (width, height) tuple up to the nearest multiple of 32 horizontally
-    and 16 vertically (as this is what the Pi's camera module does for
-    unencoded output).
-    """
-    if splitter:
-        fwidth = (width + 15) & ~15
-    else:
-        fwidth = (width + 31) & ~31
-    fheight = (height + 15) & ~15
-    return fwidth, fheight
-
-
-def now():
-    return datetime.datetime.now().time()
+from picam import PiCamera
 
 class PolarisApp:
 
@@ -30,6 +13,7 @@ class PolarisApp:
         if videofile is not None:
             print('FAKE CAMERA from %s' % videofile)
         self.gphoto = GPhotoCamera(self, videofile)
+        self.picam = PiCamera(self)
 
     def __call__(self, env, start_response):
         """
@@ -45,9 +29,9 @@ class PolarisApp:
         if path.startswith('/camera/liveview/'):
             return self.gphoto.liveview(path)
         elif path.startswith('/camera/picture/'):
-            return self.gphoto_picture(path)
-        elif path.startswith('/picamera/'):
-            return self.picamera(path)
+            return self.gphoto.picture(path)
+        elif path.startswith('/picamera/liveview/'):
+            return self.picamera.liveview(path)
         elif path == '/counter/':
             return self.counter()
         else:
@@ -78,102 +62,6 @@ class PolarisApp:
         self.start_response(http_status, headers)
         return lines()
 
-    def parse_picamera(self, path):
-        # XXX: we should handle errors somehow
-        # path is something like '/camera/raw/640x480/'
-        parts = path.split('/')
-        assert len(parts) == 5
-        root, camera, fmt, resolution, trail = parts
-        assert root == ''
-        assert camera == 'picamera'
-        assert trail == ''
-        assert fmt in ('raw', 'mjpg')
-        w, h = resolution.split('x')
-        w = int(w)
-        h = int(h)
-        fps = self.qs.get('fps', '10')
-        shutter = self.qs.get('shutter', None) # shutter speed, in seconds
-        if shutter:
-            shutter = int(float(shutter) * 10**6)
-        return fmt, w, h, fps, shutter
-
-    def picamera(self, path):
-        fmt, w, h, fps, shutter = self.parse_picamera(path)
-        w, h = raw_resolution(w, h)
-        if fmt == 'raw':
-            headers = [
-                ('Content-Type', 'video/x-raw'),
-                ('X-Width', str(w)),
-                ('X-Height', str(h)),
-            ]
-        elif fmt == 'mjpg':
-            headers = [
-                ('Content-Type', 'video/x-motion-jpeg'),
-            ]
-        self.start_response('200 OK', headers)
-        return self.frames_from_raspivid(fmt, w, h, fps, shutter)
-        #return fromfile('/home/pi/video.yuv', 'raw', w*h)
-
-    def frames_fromfile(self, fname, fmt, frame_size):
-        with open(fname, 'rb') as f:
-            yield from self.getframes(f, fmt, frame_size)
-
-    def frames_from_raspivid(self, fmt, w, h, fps, shutter):
-        if fmt == 'raw':
-            cmd = ['raspividyuv', '--luma'] # stream only the Y (luminance) data
-            frame_size = w*h
-        elif fmt == 'mjpg':
-            cmd = ['raspivid', '-cd', 'MJPEG']
-            # we don't have a fixed frame size, stream the data in chunks of 1k
-            frame_size = 1024
-
-        cmd += ['-t', '0', # no timeout, record forever
-                '-w', str(w),
-                '-h', str(h),
-                '-fps', fps,
-                '-o', '-']
-        if shutter:
-            cmd += [
-                '-ss', str(shutter)
-                ]
-        print('Executing: %s' % ' '.join(cmd))
-        p = subprocess.Popen(cmd, bufsize=0, stdout=subprocess.PIPE)
-        try:
-            yield from self.getframes(p.stdout, fmt, frame_size)
-        finally:
-            self.terminate(p)
-
-    def getframes(self, f, fmt, frame_size):
-        tstart = time.time()
-        i = 0
-        bytes_read = 0
-        while True:
-            data = f.read(frame_size)
-            if data == b'':
-                print('getframes EOF')
-                break
-            bytes_read += len(data)
-            if fmt == 'raw' and bytes_read > frame_size:
-                # got a full frame
-                print('[%5.2f %s] got frame: %d' % (time.time()-tstart, now(), i))
-                i += 1
-                bytes_read -= frame_size
-            yield data
-
-
-    def terminate(self, p, timeout=1):
-        """
-        Terminate gracefully the given process, or kill
-        """
-        print('Sending SIGTERM...')
-        try:
-            p.terminate()
-            p.wait(timeout)
-        except subprocess.TimeoutExpired:
-            print('Timeout :(, killing the process')
-            p.kill()
-        else:
-            print('Process successully terminated')
 
 def main(fname):
     from wsgiref.simple_server import make_server
