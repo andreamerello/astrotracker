6166819ad96304b722c63b32691221d740dfe898
Antonio Cuni <anto.cuni@gmail.com>
2020-05-26 14:39:22 +0100

    move the gphoto-related logic into its own file

---
+++ piware/gphotocam.py
@@ -0,0 +1,100 @@
+import os
+import time
+import subprocess
+
+class GPhotoCamera:
+
+    def __init__(self, app, videofile):
+        self.app = app
+        self.videofile = videofile
+
+    def liveview(self, path):
+        if self.videofile is not None:
+            # for testing
+            yield from self.serve_videofile(self.videofile)
+            return
+
+        # gphoto2 camera
+        cmd = ['gphoto2',
+               #'--port', 'ptpip:192.168.1.180',
+               '--set-config', 'output=TFT + PC',
+               '--capture-movie',
+               '--stdout'
+        ]
+        print('Executing: %s' % ' '.join(cmd))
+        p = subprocess.Popen(cmd, bufsize=0, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        #
+        # If gphoto2 can't find the camera, it prints some text on stdout. To
+        # catch the case, we try to read some bytes: if out1 is empty, it
+        # means that gphoto is not streaming and out0 contains the error
+        # message. Else, out0 and out1 contains "real" MJPG frames, which we
+        # need to yield to the client.
+        out0 = p.stdout.read(1024)
+        out1 = p.stdout.read(1024)
+        if out1 == b'':
+            # gphoto is not streaming anything, so it must be an error
+            stderr = p.stderr.read()
+            p.wait()
+            self.app.start_response('400 Bad Request', [])
+            yield stderr
+            yield out0
+            return
+        #
+        # if we are here, gphoto is streaming correctly, let's read the frames
+        headers = [
+            ('Content-Type', 'video/x-motion-jpeg'),
+        ]
+        self.app.start_response('200 OK', headers)
+        # yield the small data that we read preemptively
+        yield out0
+        yield out1
+        # yield the rest
+        try:
+            # I measured that a single frame is ~54k (but maybe it will be
+            # smaller if you shoot the dark sky?). We want a frame size which
+            # is small enough to avoid waiting for multiple frames, but if
+            # it's too small the rpi0 CPU can't handle all the work and we get
+            # very few FPS. The following value seems to work well empirically
+            frame_size = 1024 * 16
+            while True:
+                data = p.stdout.read(frame_size)
+                if data == b'':
+                    # gphoto stopped streaming, something is wrong?
+                    break
+                yield data
+        finally:
+            self.terminate(p)
+            # make sure to unlock the camera at the end
+            os.system('gphoto2 --set-config output=TFT')
+
+    def serve_videofile(self, fname):
+        # we assume it's a mjpg for now. Serve full frames at a constant rate
+        headers = [
+            ('Content-Type', 'video/x-motion-jpeg'),
+        ]
+        self.app.start_response('200 OK', headers)
+        FPS = 5.0
+        data = b''
+        nframes = 0
+        with open(fname, 'rb') as f:
+            while True:
+                chunk = f.read(1024)
+                if chunk == b'':
+                    return
+                data += chunk
+                a = data.find(b'\xff\xd8') # jpg_start
+                b = data.find(b'\xff\xd9') # jpg_end
+                if a != -1 and b != -1:
+                    # found a new frame!
+                    nframes += 1
+                    print('Got frame %d' % nframes)
+                    jpg_data = data[a:b+2]
+                    data = data[b+2:]
+                    yield jpg_data
+                    time.sleep(1/FPS)
+
+    def picture(self, path):
+        headers = []
+        self.app.start_response('404 NOT FOUND', headers)
+        return []
--- piware/polaris.py
+++ piware/polaris.py
@@ -4,6 +4,7 @@ import datetime
 import subprocess
 import os
 from urllib.parse import parse_qs
+from gphotocam import GPhotoCamera
 
 # stolen from picamera
 def raw_resolution(width, height, splitter=False):
@@ -26,9 +27,9 @@ def now():
 class PolarisApp:
 
     def __init__(self, videofile=None):
-        self.videofile = videofile
         if videofile is not None:
             print('FAKE CAMERA from %s' % videofile)
+        self.gphoto = GPhotoCamera(self, videofile)
 
     def __call__(self, env, start_response):
         """
@@ -42,7 +43,7 @@ class PolarisApp:
             path += '/'
         print('Handling', path)
         if path.startswith('/camera/liveview/'):
-            return self.gphoto_liveview(path)
+            return self.gphoto.liveview(path)
         elif path.startswith('/camera/picture/'):
             return self.gphoto_picture(path)
         elif path.startswith('/picamera/'):
@@ -95,98 +96,6 @@ class PolarisApp:
         if shutter:
             shutter = int(float(shutter) * 10**6)
         return fmt, w, h, fps, shutter
-
-    def serve_videofile(self, fname):
-        # we assume it's a mjpg for now. Serve full frames at a constant rate
-        headers = [
-            ('Content-Type', 'video/x-motion-jpeg'),
-        ]
-        self.start_response('200 OK', headers)
-        FPS = 5.0
-        data = b''
-        nframes = 0
-        with open(fname, 'rb') as f:
-            while True:
-                chunk = f.read(1024)
-                if chunk == b'':
-                    return
-                data += chunk
-                a = data.find(b'\xff\xd8') # jpg_start
-                b = data.find(b'\xff\xd9') # jpg_end
-                if a != -1 and b != -1:
-                    # found a new frame!
-                    nframes += 1
-                    print('Got frame %d' % nframes)
-                    jpg_data = data[a:b+2]
-                    data = data[b+2:]
-                    yield jpg_data
-                    time.sleep(1/FPS)
-
-
-    def gphoto_liveview(self, path):
-        if self.videofile is not None:
-            # for testing
-            yield from self.serve_videofile(self.videofile)
-            return
-
-        # gphoto2 camera
-        cmd = ['gphoto2',
-               #'--port', 'ptpip:192.168.1.180',
-               '--set-config', 'output=TFT + PC',
-               '--capture-movie',
-               '--stdout'
-        ]
-        print('Executing: %s' % ' '.join(cmd))
-        p = subprocess.Popen(cmd, bufsize=0, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        #
-        # If gphoto2 can't find the camera, it prints some text on stdout. To
-        # catch the case, we try to read some bytes: if out1 is empty, it
-        # means that gphoto is not streaming and out0 contains the error
-        # message. Else, out0 and out1 contains "real" MJPG frames, which we
-        # need to yield to the client.
-        out0 = p.stdout.read(1024)
-        out1 = p.stdout.read(1024)
-        if out1 == b'':
-            # gphoto is not streaming anything, so it must be an error
-            stderr = p.stderr.read()
-            p.wait()
-            self.start_response('400 Bad Request', [])
-            yield stderr
-            yield out0
-            return
-        #
-        # if we are here, gphoto is streaming correctly, let's read the frames
-        headers = [
-            ('Content-Type', 'video/x-motion-jpeg'),
-        ]
-        self.start_response('200 OK', headers)
-        # yield the small data that we read preemptively
-        yield out0
-        yield out1
-        # yield the rest
-        try:
-            # I measured that a single frame is ~54k (but maybe it will be
-            # smaller if you shoot the dark sky?). We want a frame size which
-            # is small enough to avoid waiting for multiple frames, but if
-            # it's too small the rpi0 CPU can't handle all the work and we get
-            # very few FPS. The following value seems to work well empirically
-            frame_size = 1024 * 16
-            while True:
-                data = p.stdout.read(frame_size)
-                if data == b'':
-                    # gphoto stopped streaming, something is wrong?
-                    break
-                yield data
-        finally:
-            self.terminate(p)
-            # make sure to unlock the camera at the end
-            os.system('gphoto2 --set-config output=TFT')
-
-    def gphoto_picture(self, path):
-        headers = []
-        self.start_response('404 NOT FOUND', headers)
-        return []
 
     def picamera(self, path):
         fmt, w, h, fps, shutter = self.parse_picamera(path)
