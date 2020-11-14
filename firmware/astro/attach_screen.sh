dadef739881e335696dd156027177b43b48eb3ab
Antonio Cuni <anto.cuni@gmail.com>
2020-11-14 17:13:57 +0100

    use CTRL-C to exit the screen

--- firmware/astro/attach_screen.sh
+++ firmware/astro/attach_screen.sh
@@ -1,1 +1,6 @@
-sudo screen /dev/ttyACM0 115200
+# special configuration of screen: press CTRL+C to quit the screen
+cat > /tmp/myscreenrc <<EOF
+bindkey "^C" quit
+EOF
+
+sudo screen -c /tmp/myscreenrc /dev/ttyACM0 115200
