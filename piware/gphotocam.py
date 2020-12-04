33a88baecaadc22304529cb2481a14430ba3a6a2
Antonio Cuni <anto.cuni@gmail.com>
2020-12-04 23:00:11 +0000

    move iter_mjpg to utils.py

--- piware/gphotocam.py
+++ piware/gphotocam.py
@@ -2,33 +2,9 @@ import os
 import time
 import subprocess
 import collections
-from utils import terminate
 from pathlib import Path
 import threading
-
-def iter_mjpg(f, data=b''):
-    """
-    Iterates over all the frames in a MJPG stream
-    """
-    # I measured that a single frame is ~54k (but maybe it will be
-    # smaller if you shoot the dark sky?). We want a frame size which
-    # is small enough to avoid waiting for multiple frames, but if
-    # it's too small the rpi0 CPU can't handle all the work and we get
-    # very few FPS. The following value seems to work well empirically
-    chunk_size = 1024 * 16
-    while True:
-        chunk = f.read(chunk_size)
-        if chunk == b'':
-            return
-        data += chunk
-        a = data.find(b'\xff\xd8') # jpg_start
-        b = data.find(b'\xff\xd9') # jpg_end
-        if a != -1 and b != -1:
-            jpg_data = data[a:b+2]
-            data = data[b+2:]
-            yield jpg_data
-
-
+from utils import terminate, iter_mjpg
 
 class GPhotoThread:
 
